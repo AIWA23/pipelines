8f5eb87002422fdd1b680d7df848ce58050dddea
AW <aashishwagle@gmail.com>
2022-02-01 12:51:59 +0000

    Update Startup.cs

--- pipelines/Startup.cs
+++ pipelines/Startup.cs
@@ -14,7 +14,7 @@ namespace pipelines
     public class Startup
     {
         public Startup(IConfiguration configuration)
-        {
+        {   // Sample Change
             Configuration = configuration;
         }
 
